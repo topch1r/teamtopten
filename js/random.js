8351a780cf3e8f4286272625ef2f0e536c7cfdce
topch1r <75265061+topch1r@users.noreply.github.com>
2023-05-17 12:30:04 +0300

    Add files via upload
    team

---
+++ js/random.js
@@ -0,0 +1,37 @@
+
+function ClickFunction () {
+    const max = 10;
+    res = Math.round(Math.random() * max);
+    switch(res) {
+        case 1:
+            location.href='./top1.html';
+            break;
+        case 2:
+            location.href='./top2.html';
+            break;
+        case 3:
+            location.href='./top3.html';
+            break;
+        case 4:
+            location.href='./top4.html';
+            break;
+        case 5:
+            location.href='./top5.html';
+            break;
+        case 6:
+            location.href='./top6.html';
+            break;
+        case 7:
+            location.href='./top7.html';
+            break;
+        case 8:
+            location.href='./top8.html';
+            break;
+        case 9:
+            location.href='./top9.html';
+            break;
+        case 10:
+            location.href='./top10.html';
+            break;
+    }
+}
